9327b97982c65301f6214ecd2594bac4bd35888b
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2017-12-27 11:06:16 +0100

    Modify script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 var url = "http://api.icndb.com/jokes/random";
 
-$("p").load(url, getJoke);
+$(document).ready(getJoke);
 
 var button = document.getElementById("get-joke");
-button.addEventListener("click", function() {
-	getJoke();
-});
+button.addEventListener("click", getJoke);
 
 var paragraph = document.getElementById("joke");
 
