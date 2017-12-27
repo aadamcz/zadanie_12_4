b2f3588dcbaa7b7ddb2a73ffe8999a39338b67b9
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2017-12-27 10:49:24 +0100

    Change script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-var url = 'http://api.icndb.com/jokes/random';
+var url = "http://api.icndb.com/jokes/random";
 
-$('p').load(url, getJoke);
+$("p").load(url, getJoke);
 
-var button = document.getElementById('get-joke');
-button.addEventListener('click', function(){
+var button = document.getElementById("get-joke");
+button.addEventListener("click", function() {
 	getJoke();
 });
 
-var paragraph = document.getElementById('joke');
+var paragraph = document.getElementById("joke");
 
 function getJoke() {
 	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url);
-	xhr.addEventListener('load', function(){
+	xhr.open("GET", url);
+	xhr.addEventListener("load", function() {
 		var response = JSON.parse(xhr.response);
 		paragraph.innerHTML = response.value.joke;
 	});
